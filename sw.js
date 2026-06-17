6c4b0be50f4c27f0c867164aa791efe6f413c15f
Jaykuwer-Yadav <jaykuwer.yadav@gmail.com>
2026-06-17 09:29:29 +0530

    Fix sw.js InvalidAccessError on install event

--- sw.js
+++ sw.js
@@ -1,13 +1,24 @@
 const CACHE_NAME = 'addmyproperty-v1';
 const ASSETS = [
-  './',
-  './index.html'
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      // Use individual fetch and put to avoid 'Entry already exists' if any URL resolves similarly
+      return Promise.all(
+        ASSETS.map((url) => {
+          return fetch(url).then((response) => {
+            if (response.ok) {
+              return cache.put(url, response);
+            }
+          }).catch((error) => {
+            console.error('Failed to cache:', url, error);
+          });
+        })
+      );
     })
   );
   self.skipWaiting();
